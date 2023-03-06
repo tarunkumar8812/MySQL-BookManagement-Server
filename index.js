ee82c8e4cd46c21a089d384552bf64d6fa1429a4
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-07 02:51:57 +0530

    /

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const router = require("./src/routes/route.js")
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const pool = require("./src/db.js")
 
 
-app.use("/", router)
+app.use("/", async (req, res) => {
+    const result = await pool.query(` SELECT * FROM product `)
+    console.log(result[0]);
+    return res.json({ msg: "hello world", data: result[0] })
+})
 
 
 
-app.use("/*", (req, res) => {
-    return res.status(404).send({ status: false, message: `Invalid Path url` })
+// app.use("/*", (req, res) => {
+//     return res.status(404).send({ status: false, message: `Invalid Path url` })
 
-});
+// });
 
 
 app.listen(process.env.PORT || 3000, function () {
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { getAllUsers, getUserById } = require('../controllers/controller.js')
 
 
+
 router.get('/', getAllUsers)
 router.get('/getUserById/:id', getUserById)
 
