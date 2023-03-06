106dae6fd4a9bb1d4cbc10b2e8a98af21428ae5a
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-07 02:44:08 +0530

    vercel.json is  added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = require("./src/routers/router.js")
+const router = require("./src/routes/route.js")
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
