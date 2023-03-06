a347e5349693b075e2d1c33c1b1cd2353f1b44d6
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-07 02:54:09 +0530

    /

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require("express");
-const router = require("./src/routes/route.js")
+const router = require("./src/routers/router.js")
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const pool = require("./src/db.js")
 
 
-app.use("/", async (req, res) => {
-    const result = await pool.query(` SELECT * FROM product `)
-    console.log(result[0]);
-    return res.json({ msg: "hello world", data: result[0] })
-})
+app.use("/", router)
 
 
 
-// app.use("/*", (req, res) => {
-//     return res.status(404).send({ status: false, message: `Invalid Path url` })
+app.use("/*", (req, res) => {
+    return res.status(404).send({ status: false, message: `Invalid Path url` })
 
-// });
+});
 
 
 app.listen(process.env.PORT || 3000, function () {
--- src/routers/router.js
+++ src/routers/router.js
@@ -3,8 +3,7 @@ const router = express.Router()
 const { getAllUsers, getUserById } = require('../controllers/controller.js')
 
 
-
-router.get('/', getAllUsers)
+router.get('/getAllUsers', getAllUsers)
 router.get('/getUserById/:id', getUserById)
 
 module.exports = router
