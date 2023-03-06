d50c16c81cf98c205b781f687e3939cf6b65eaab
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-07 02:48:16 +0530

    /

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { getAllUsers, getUserById } = require('../controllers/controller.js')
 
 
-router.get('/getAllUsers', getAllUsers)
+router.get('/', getAllUsers)
 router.get('/getUserById/:id', getUserById)
 
 module.exports = router
